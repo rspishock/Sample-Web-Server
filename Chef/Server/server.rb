f554525ad8cf873bef5a69ae328b4afbc07d5a85
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-11-05 19:37:38 -0500

    Add additional services to server.rb

--- Chef/Server/server.rb
+++ Chef/Server/server.rb
@@ -1,17 +1,56 @@
+# Packages
 package 'httpd' do
     action :install
 end
 
+package 'ssh-server' do 
+	action :install
+end
+
+package 'ssh-client' do 
+	action :install
+end
+
+package 'ntp' do
+	action :install
+end
+
+package 'tree' do
+	action :install
+end
+
+package 'vim-enhanced' do
+	action :install
+end
+
+
+# Sample web page
 template '/var/www/html/index.html' do
     content 'index.html.erb'
 
     action :create
 end
 
+
+# Services
 service 'httpd' do
     action [ :enable, :start ]
 end
 
+service 'ssh-server' do
+    action [ :enable, :start ]
+end
+
+service 'ssh-client' do
+    action [ :enable, :start ]
+end
+
+service 'ntp' do
+    action [ :enable, :start ]
+end
+
+
+# Files
 file '/etc/motd' do
     content "ALERT! You are entering into a secured area! Your IP, Login Time, Username has been noted and has been sent to the server administrator!
     This service is restricted to authorized users only. All activities on this system are logged.
