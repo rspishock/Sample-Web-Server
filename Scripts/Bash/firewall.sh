3818e84e15866d6159816a7f51baff12a75689e6
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-12-18 19:32:39 -0500

    Block outgoing SSH connection

--- Scripts/Bash/firewall.sh
+++ Scripts/Bash/firewall.sh
@@ -4,3 +4,6 @@
 iptables -A INPUT -p TCP 80 -j ACCEPT
 iptables -A INPUT -p TCP 443 -j ACCEPT
 iptables -A INPUT -p TCP 22 -j ACCEPT
+
+# drop outgoing ssh connections
+iptables -A OUTPUT -p TCP --dport 22 -j DROP
