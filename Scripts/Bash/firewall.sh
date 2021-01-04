d641bc04929f13855d9188e3cc774c805fde9ca0
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2021-01-03 20:03:05 -0500

    Code refactor

--- Scripts/Bash/firewall.sh
+++ Scripts/Bash/firewall.sh
@@ -6,8 +6,8 @@ iptables -F
 # accept incoming http, https, and ssh connections
 iptables -A INPUT -p TCP --dport 80 -j ACCEPT
 iptables -A INPUT -p TCP --dport 443 -j ACCEPT
-# update IP address to match IP for permissable system
-iptables -A INPUT -p TCP --dport 22 -s 192.168.1.150 -j ACCEPT
+# replace <SAMPLE IP> with IP address from workstation
+iptables -A INPUT -p TCP --dport 22 -s <SAMPLE IP> -j ACCEPT
 iptables -A INPUT -p TCP --dport 22 -j DROP
 
 # drop outgoing ssh connections
