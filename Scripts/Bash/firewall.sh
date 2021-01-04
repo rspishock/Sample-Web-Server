3b840f055cc4ba1cb60299db9abe11a29362c751
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2021-01-03 20:01:46 -0500

    Update to SSH drop rules

--- Scripts/Bash/firewall.sh
+++ Scripts/Bash/firewall.sh
@@ -8,6 +8,7 @@ iptables -A INPUT -p TCP --dport 80 -j ACCEPT
 iptables -A INPUT -p TCP --dport 443 -j ACCEPT
 # update IP address to match IP for permissable system
 iptables -A INPUT -p TCP --dport 22 -s 192.168.1.150 -j ACCEPT
+iptables -A INPUT -p TCP --dport 22 -j DROP
 
 # drop outgoing ssh connections
 iptables -A OUTPUT -p TCP --dport 22 -j DROP
