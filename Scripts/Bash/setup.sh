64c51f363bedb545ff8f967d5f57dfe7ad0dd5fc
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2021-01-15 19:18:23 -0500

    Update setup script

--- Scripts/Bash/setup.sh
+++ Scripts/Bash/setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Create directory structure
+cd ~; mkdir Setup; cd Setup; 
+
+# Pull repository
+git pull https://github.com/rspishock/Sample-Web-Server
+
 # Copy Splunk to /opt/
 cp splunk-8.1.1-08187535c166-Linux-x86_64.tgz /opt/
 
